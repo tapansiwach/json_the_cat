782362c358ae001fb9469436582975b57130edc3
Tapan Siwach <siwach.tapan@gmail.com>
2021-08-26 08:12:08 -0400

    refactor: move output logic to index.js and but keep search logic within breedFetcher.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,21 +1,28 @@
 const request = require("request");
 
 const apiEndpoint = "https://api.thecatapi.com/v1/breeds/search";
-const breedName = process.argv.slice(2)[0];
-const apiUrl = `${apiEndpoint}?name=${breedName}`;
 
-request(apiUrl, (err, response, body) => {
-  if (err) {
-    console.log(err);
-    return;
-  }
+const fetchBreedDescription = function(breedName, callback) {
+  const apiUrl = `${apiEndpoint}?name=${breedName}`;
 
-  const data = JSON.parse(body);
-  if (data.length === 0) {
-    console.log("breed not found");
-    return;
-  }
+  request(apiUrl, (err, response, body) => {
 
-  console.log(data[0].description);
+    if (err) {
+      callback(err, null);
+      return;
+    }
 
-});
+    const data = JSON.parse(body);
+    if (data.length === 0) {
+      callback(null, null);
+      return;
+    }
+
+    const description = data[0].description;
+    callback(null, description);
+
+  });
+
+};
+
+module.exports = { fetchBreedDescription };
